38638de5dcee3adec60ada0cc24bffb6bab6fd7a
Yash-angularminds <97215057+Yash-angularminds@users.noreply.github.com>
2022-04-29 18:34:54 +0530

    Update Feed.js

--- FrontEnd/src/Components/Feed.js
+++ FrontEnd/src/Components/Feed.js
@@ -1,5 +1,11 @@
-import React,{ useState, useEffect } from 'react';
-import { Card,CardHeader,styled,CardMedia,IconButton,red,FavoriteIcon,ShareIcon,ExpandMoreIcon,MoreVertIcon,CardContent,CardActions,Collapse,Avatar,Box,Container,TextField,Button,Typography,Link } from '@mui/material';
+import React from 'react'
+import { Card,CardHeader,styled,CardMedia,IconButton,CardContent,CardActions,Collapse,Avatar,Box,Container,TextField,Button,Typography,Link } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { red } from '@mui/material/colors';
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -12,77 +18,52 @@ const ExpandMore = styled((props) => {
 }));
 
 function Feed() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
 
- 
-  
-  
-    const [expanded, setExpanded] = React.useState(false);
-  
-    const handleExpandClick = () => {
-      setExpanded(!expanded);}
+  const [expanded, setExpanded] = React.useState(false);
 
-  useEffect(() => {
-    if (selectedImage) {
-      setImageUrl(URL.createObjectURL(selectedImage));
-    }
-  }, [selectedImage]);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);}
 
   return (
-    <>
-    <React.Fragment>
-        <Container maxWidth="xl" sx={{ bgcolor: 'whitesmoke', display: "flex", justifyContent: "center" }}
-            >
-                <Card sx={{ maxWidth: "50%", mt: 10, textAlign: 'center', overflow: "auto" }}>
-                <CardHeader
-                    subheader="New Post"
-                    sx={{ color: "primary" }}
-                />
-                <Box
-                component="form"
-                sx={{'& > :not(style)': { mx: 1,my: 1}
-                }}
-                >
-                <br />
-                <input
-                  accept="image/*"
-                  type="file"
-                  id="select-image"
-                  style={{ display: 'none' }}
-                  onChange={e => setSelectedImage(e.target.files[0])}
-                />
-                <label htmlFor="select-image">
-                  <Button variant="outlined" color="primary" component="span">
-                    Img
-                  </Button>
-                </label>
-                {imageUrl && selectedImage && (
-                  <Box mt={2} textAlign="center">
-                    <div>Post Preview:</div>
-                    <img src={imageUrl} alt={selectedImage.name} height="150px" />
-                  </Box>
-                )}
-                
-                <TextField
-                id="name"
-                label="Write a Caption.."
-                color="primary"
-                variant="outlined"
-                size="small"
-                sx={{ width: 300, color: "primary" }}
-                required
-                />
-                <br />
-                <Button variant="contained" color="primary" component="span">
-                  Post
-                </Button>
-                </Box>
-                </Card>
-        </Container>
-    </React.Fragment>
-    </>
-  );
-};
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image="/home/am-pc-14/Pictures/Wallpapers/398086.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Comments:</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  )
+}
 
-export default Feed;
+export default Feed
+
+  
