19201a63465e01de6567ed5646cabbc6eb39ce43
Yash-angularminds <97215057+Yash-angularminds@users.noreply.github.com>
2022-04-29 18:36:12 +0530

    Update UploadFeed.js

--- FrontEnd/src/Components/UploadFeed.js
+++ FrontEnd/src/Components/UploadFeed.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import { Card,CardHeader,styled,CardMedia,IconButton,red,FavoriteIcon,ShareIcon,ExpandMoreIcon,MoreVertIcon,CardContent,CardActions,Collapse,Avatar,Box,Container,TextField,Button,Typography,Link } from '@mui/material';
 
-function UploadFeed() {
+
+function Feed() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
+
   return (
-    <div>UploadFeed</div>
-  )
-}
+    <>
+    <React.Fragment>
+        <Container maxWidth="xl" sx={{ bgcolor: 'whitesmoke', display: "flex", justifyContent: "center" }}
+            >
+                <Card sx={{ maxWidth: "50%", mt: 10, textAlign: 'center', overflow: "auto" }}>
+                <CardHeader
+                    subheader="New Post"
+                    sx={{ color: "primary" }}
+                />
+                <Box
+                component="form"
+                sx={{'& > :not(style)': { mx: 1,my: 1}
+                }}
+                >
+                <br />
+                <input
+                  accept="image/*"
+                  type="file"
+                  id="select-image"
+                  style={{ display: 'none' }}
+                  onChange={e => setSelectedImage(e.target.files[0])}
+                />
+                <label htmlFor="select-image">
+                  <Button variant="outlined" color="primary" component="span">
+                    Img
+                  </Button>
+                </label>
+                {imageUrl && selectedImage && (
+                  <Box mt={2} textAlign="center">
+                    <div>Post Preview:</div>
+                    <img src={imageUrl} alt={selectedImage.name} height="150px" />
+                  </Box>
+                )}
+                
+                <TextField
+                id="name"
+                label="Write a Caption.."
+                color="primary"
+                variant="outlined"
+                size="small"
+                sx={{ width: 300, color: "primary" }}
+                required
+                />
+                <br />
+                <Button variant="contained" color="primary" component="span">
+                  Post
+                </Button>
+                </Box>
+                </Card>
+        </Container>
+        
+    </React.Fragment>
+    </>
+  );
+};
 
-export default UploadFeed
+export default UploadFeed;
